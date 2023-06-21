c1aa22ff492f413d7f93d7c970d27166870e2369
Rob <kubrob@gmail.com>
2023-06-21 21:43:01 +0200

    Update NavBar.js
    change name

--- components/layout/NavBar.js
+++ components/layout/NavBar.js
@@ -30,7 +30,7 @@ export function NavBar() {
             <Icon size="lg">
               <Icons.Logo />
             </Icon>
-            <Text className={styles.text}>umami</Text>
+            <Text className={styles.text}>stati</Text>
           </div>
           <div className={styles.links}>
             {links.map(({ url, label }) => {
